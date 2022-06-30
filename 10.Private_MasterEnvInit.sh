f08c028c55c21562f25876a6f269badc3bbe2066
redicarus-jeong <redicarus.jeong@gmail.com>
2022-06-30 16:50:03 +0900

    env file update

--- 10.Private_MasterEnvInit.sh
+++ 10.Private_MasterEnvInit.sh
@@ -1,61 +1,44 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bashHARBOR_HOSTNAME
 
 ### this script is Preinstall Kubernetes Masters
 ### date : 2022-06-16
 ### author : redicarus.jeong
 
-CurrentPath=$(pwd)
-ScriptHomeDir=${CurrentPatch}/script
-RepositoryHostname="repository"
-HarborHostname="harbor"
-RepoPort=3777
-
-### Incloud ENV File ###
-HarborVersion="1.10.6"
-CubeVersion="1.21"
-MainDir="/APP"
-CubeWork="${MainDir}/acorn"
-CubeDir="${CubeWork}/cube"
-CubeData="${CubeWork}/data"
-CubeTmp="${MainDir}/acornsoft"
-CubeExec="${MainDir}/cocktail"
-AWSServer="disable"
-
-if [ ! -d ${CubeWork}/cube/temp ]; then 
-  sudo mkdir -p ${CubeWork}/cube/temp
+if [ ! -d ${CUBE_WORK}/cube/temp ]; then 
+  sudo mkdir -p ${CUBE_WORK}/cube/temp
 fi
 
 ##### 1. ADD Package Repo ###
-echo "====> 1. ADD Package Repo : script call = ${CurrentPath}/script/91.PKGRepoConnectSetup.sh ###"
-if [ -f ${CurrentPath}/91.PKGRepoConnectSetup.sh ]; then
-    sudo sh ${CurrentPath}/91.PKGRepoConnectSetup.sh
+echo "====> 1. ADD Package Repo : script call = ${CURRENT_PATH}/script/91.PKGRepoConnectSetup.sh ###"
+if [ -f ${CURRENT_PATH}/91.PKGRepoConnectSetup.sh ]; then
+    sudo sh ${CURRENT_PATH}/91.PKGRepoConnectSetup.sh
 else
-    sudo sh ${CurrentPath}/script/91.PKGRepoConnectSetup.sh
+    sudo sh ${CURRENT_PATH}/script/91.PKGRepoConnectSetup.sh
 fi
 
 
-SystemDefaultRPM=("net-tools" "wget" "epel-release" "openssl11" "nfs-utils" "yum-utils" "device-mapper-persistent-data" "lvm2" "jq")
+SYSTEM_DEFAULT_RPM=("net-tools" "wget" "epel-release" "openssl11" "nfs-utils" "yum-utils" "device-mapper-persistent-data" "lvm2" "jq")
 ##### 1. System Default Package install & cube temp directory create
 echo "====> 1. System Default Package install & cube temp directory create"
 echo " ---> 1-1. cube temp directory create"
-if [ ! -d ${CubeTmp}/temp ]; then
-    sudo mkdir -p ${CubeTmp}/temp
+if [ ! -d ${CUBE_TEMP}/temp ]; then
+    sudo mkdir -p ${CUBE_TEMP}/temp
 fi
-if [ ! -d ${CubeWork} ]; then
-    sudo mkdir -p ${CubeWork}
+if [ ! -d ${CUBE_WORK} ]; then
+    sudo mkdir -p ${CUBE_WORK}
 fi
 
 echo " ---> 1-2. System Default Package install "
-for rpmname in ${SystemDefaultRPM[@]}
+for RPM_NAME in ${SYSTEM_DEFAULT_RPM[@]}
     do
-	    InstallResult=$(rpm -qa | grep ${rpmname} | wc -l)
-		if [ ${InstallResult} -ge 1 ]; then
-		    echo " >>>> yum re-install : ${rpmname}"
-			sudo yum reinstall -y ${rpmname}
+	    INSTALL_RESULT=$(rpm -qa | grep ${RPM_NAME} | wc -l)
+		if [ ${INSTALL_RESULT} -ge 1 ]; then
+		    echo " >>>> yum re-install : ${RPM_NAME}"
+			sudo yum reinstall -y ${RPM_NAME}
 			
-		elif [ ${InstallResult} -eq 0 ]; then
-		    echo " >>>> yum install : ${rpmname}"
-		    sudo yum install -y ${rpmname}
+		elif [ ${INSTALL_RESULT} -eq 0 ]; then
+		    echo " >>>> yum install : ${RPM_NAME}"
+		    sudo yum install -y ${RPM_NAME}
 		else
 		    echo " >>>> yum pkg rpm install failed."
 			exit 0
@@ -64,34 +47,34 @@ done
 
 
 ##### 2. ETCD & Helm
-WgetBinary=("helm" "sshpass" "etcd")
+WGET_BINARY=("helm" "sshpass" "etcd")
 echo "====> 2. wget etcd & Helm binary from PKG Repository"
-RepositoryIP=$(grep ${RepositoryHostname} /etc/hosts | awk 'NR==1 {print $1}')
-RepoPingCount=$(ping -c 3 ${RepositoryIP} | grep received |cut -d',' -f2|awk '{print $1}' | sed 's/ //g')
-if [  $RepoPingCount -ne 3 ]; then
-    echo " >>>> Check CentOS-7.9 Private RPM Repository Server IP(=${RepositoryIP})"
+REPOSITORY_IP=$(grep ${REPOSITORY_HOSTNAME} /etc/hosts | awk 'NR==1 {print $1}')
+REPOSITORY_PING_COUNT=$(ping -c 3 ${REPOSITORY_IP} | grep received |cut -d',' -f2|awk '{print $1}' | sed 's/ //g')
+if [  $REPOSITORY_PING_COUNT -ne 3 ]; then
+    echo " >>>> Check CentOS-7.9 Private RPM Repository Server IP(=${REPOSITORY_IP})"
     exit 1
 else
-    echo " >>>> wget ${WgetBinary[0]} "
-    if [ ! -f ${CubeTmp}/cube/binary/${WgetBinary[0]} ]; then
-	    sudo wget http://${RepositoryIP}:${RepoPort}/binary/${WgetBinary[0]}  -P  ${CubeTmp}/cube/binary/
-        sudo chmod ug+x ${CubeTmp}/cube/binary/${WgetBinary[0]}
-        sudo cp -pf ${CubeTmp}/cube/binary/${WgetBinary[0]}  /usr/local/bin/
+    echo " >>>> wget ${WGET_BINARY[0]} "
+    if [ ! -f ${CUBE_TEMP}/cube/binary/${WGET_BINARY[0]} ]; then
+	    sudo wget http://${REPOSITORY_IP}:${REPO_PORT}/binary/${WGET_BINARY[0]}  -P  ${CUBE_TEMP}/cube/binary/
+        sudo chmod ug+x ${CUBE_TEMP}/cube/binary/${WGET_BINARY[0]}
+        sudo cp -pf ${CUBE_TEMP}/cube/binary/${WGET_BINARY[0]}  /usr/local/bin/
     fi
 
-    echo " >>>> wget ${WgetBinary[1]} "
-    if [ ! -f ${CubeTmp}/cube/binary/${WgetBinary[1]} ]; then
-	    sudo wget http://${RepositoryIP}:${RepoPort}/binary/${WgetBinary[1]}  -P  ${CubeTmp}/cube/binary/
-        sudo chmod ug+x ${CubeTmp}/cube/binary/${WgetBinary[1]}
-        sudo cp -pf ${CubeTmp}/cube/binary/${WgetBinary[1]}  /usr/local/bin/
+    echo " >>>> wget ${WGET_BINARY[1]} "
+    if [ ! -f ${CUBE_TEMP}/cube/binary/${WGET_BINARY[1]} ]; then
+	    sudo wget http://${REPOSITORY_IP}:${REPO_PORT}/binary/${WGET_BINARY[1]}  -P  ${CUBE_TEMP}/cube/binary/
+        sudo chmod ug+x ${CUBE_TEMP}/cube/binary/${WGET_BINARY[1]}
+        sudo cp -pf ${CUBE_TEMP}/cube/binary/${WGET_BINARY[1]}  /usr/local/bin/
     fi
     
-    echo " >>>> wget ${WgetBinary[2]} "
-    if [ ! -f ${CubeTmp}/cube/etcd/${WgetBinary[2]} ]; then
-        echo " >>>> wget ${WgetBinary[0]} failed"
-        sudo wget http://${RepositoryIP}:${RepoPort}/etcd/${WgetBinary[2]}  -P  ${CubeTmp}/cube/etcd/
-		sudo chmod ug+x ${CubeTmp}/cube/etcd/${WgetBinary[2]}
-        sudo cp -pf ${CubeTmp}/cube/etcd/${WgetBinary[2]}  /usr/local/bin/
+    echo " >>>> wget ${WGET_BINARY[2]} "
+    if [ ! -f ${CUBE_TEMP}/cube/etcd/${WGET_BINARY[2]} ]; then
+        echo " >>>> wget ${WGET_BINARY[0]} failed"
+        sudo wget http://${REPOSITORY_IP}:${REPO_PORT}/etcd/${WGET_BINARY[2]}  -P  ${CUBE_TEMP}/cube/etcd/
+		sudo chmod ug+x ${CUBE_TEMP}/cube/etcd/${WGET_BINARY[2]}
+        sudo cp -pf ${CUBE_TEMP}/cube/etcd/${WGET_BINARY[2]}  /usr/local/bin/
     fi
 
 fi    
@@ -99,8 +82,8 @@ fi
 
 ### 3. containerd running check & install
 echo "====> 3. containerd running check & install"
-ActiveRes=$(systemctl is-active containerd)
-if [ "${ActiveRes}" = "active" ] && [ $(rpm -qa|grep containerd|wc -l) -eq 0 ]; then
+ACTIVE_RES=$(systemctl is-active containerd)
+if [ "${ACTIVE_RES}" = "active" ] && [ $(rpm -qa|grep containerd|wc -l) -eq 0 ]; then
     echo " >>>> Containerd running"
 else
     echo " >>>> Containerd.io install"
@@ -110,15 +93,15 @@ fi
 
 ##### 4. kubernate : kubeadm / kubectl / kubelet check & install
 echo "====> 4. kubernate : kubeadm / kubectl / kubelet check & install"
-KubeTools=("kubeadm" "kubectl" "kubelet")
-for KubeName in ${KubeTools[@]}
+KUBE_TOOLS=("kubeadm" "kubectl" "kubelet")
+for KUBE_NAME in ${KUBE_TOOLS[@]}
     do
-        KubeVersion=$(sudo yum --showduplicate list ${KubeName} --disableexcludes=kubernetes | grep ${CubeVersion} | awk {'print $2'} | sort -t '.' -k3 -n | uniq | tail -n 1)
-        if [ $(echo ${KubeVersion}) -ge 5]; then
-            echo "  >>> yum install ${KubeName}-${KubeVersion}"
-            sudo yum install -y ${KubeName}-${KubeVersion}  --disableexcludes=kubernetes 
+        KUBE_VERSION=$(sudo yum --showduplicate list ${KUBE_NAME} --disableexcludes=kubernetes | grep ${CUBE_VER} | awk {'print $2'} | sort -t '.' -k3 -n | uniq | tail -n 1)
+        if [ $(echo ${KUBE_VERSION}) -ge 5]; then
+            echo "  >>> yum install ${KUBE_NAME}-${KUBE_VERSION}"
+            sudo yum install -y ${KUBE_NAME}-${KUBE_VERSION}  --disableexcludes=kubernetes 
         else
-            echo "  >>> Not found ${KubeName}-${KubeVersion} in PKG Repository(hostname=${RepositoryHostname})"
+            echo "  >>> Not found ${KUBE_NAME}-${KUBE_VERSION} in PKG Repository(hostname=${REPOSITORY_HOSTNAME})"
             exit 1
         fi
 done
@@ -126,29 +109,29 @@ done
 
 ##### 5. Certificate ca.crt copy in Harbor Registry  
 echo "===+> 5. Certificate ca.crt copy in Harbor Registry "
-HarborURL=$(grep ${HarborHostname} /etc/hosts | awk 'NR==1 {print $2}')
-HarborPingCount=$(ping -c 3 ${HarborURL} | grep received |cut -d',' -f2|awk '{print $1}' | sed 's/ //g')
-if [  $HarborPingCount -ne 3 ]; then
-    echo " >>>> Check Harbor Registry Server IP(=${HarborURL})"
+HARBOR_URL=$(grep ${HARBOR_HOSTNAME} /etc/hosts | awk 'NR==1 {print $2}')
+HARBOR_PING_COUNT=$(ping -c 3 ${HARBOR_URL} | grep received |cut -d',' -f2|awk '{print $1}' | sed 's/ //g')
+if [  $HARBOR_PING_COUNT -ne 3 ]; then
+    echo " >>>> Check Harbor Registry Server IP(=${HARBOR_URL})"
     exit 1
 else
-    if [ ! -d /etc/docker/certs.d/${HarborURL} ]; then
-        sudo mkdir -p /etc/docker/certs.d/${HarborURL}
+    if [ ! -d /etc/docker/certs.d/${HARBOR_URL} ]; then
+        sudo mkdir -p /etc/docker/certs.d/${HARBOR_URL}
     fi
-    echo "  >>> https://${HarborURL}/ca.crt copy to /etc/docker/certs.d/${HarborURL}/"
-    sudo wget  https://${HarborURL}/ca.crt  --no-check-certificate  -P  /etc/docker/certs.d/${HarborURL}/
+    echo "  >>> https://${HARBOR_URL}/ca.crt copy to /etc/docker/certs.d/${HARBOR_URL}/"
+    sudo wget  https://${HARBOR_URL}/ca.crt  --no-check-certificate  -P  /etc/docker/certs.d/${HARBOR_URL}/
 fi    
 
 
 ### 6. Firewalld disable
 echo "====> 6. firewalld disable"
 echo "#######################################################################################################"
-IsActive=$(systemctl is-active firewalld)
-IsEnable=$(systemctl is-enabled firewalld)
-if [ "${IsActive}" = "active" ]; then
+IS_ACTIVE=$(systemctl is-active firewalld)
+IS_ENABLE=$(systemctl is-enabled firewalld)
+if [ "${IS_ACTIVE}" = "active" ]; then
     sudo systemctl stop firewalld
 fi
-if [ "${IsEnable}" = "enabled" ]; then
+if [ "${IS_ENABLE}" = "enabled" ]; then
     sudo systemctl disable firewalld
 fi
 
@@ -156,9 +139,9 @@ fi
 ### 7. Selinux permissive (disable)
 echo "====> 7. Selinux mode change to permissive"
 echo "#######################################################################################################"
-SeLinuxInFileMode=$(grep ^'SELINUX=' /etc/selinux/config | cut -d'=' -f2)
+SELINUX_FILE_MODE=$(grep ^'SELINUX=' /etc/selinux/config | cut -d'=' -f2)
 sudo setenforce 0
-if [ "${SeLinuxInFileMode}" = "enforcing" ]; then
+if [ "${SELINUX_FILE_MODE}" = "enforcing" ]; then
     sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 fi
 
